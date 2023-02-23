6181260a26bbd9e397a45974e6cc9a347acb7fa0
Felipe Hoyos <felipe02-98@hotmail.com>
2023-02-23 14:51:59 -0500

    insertas el commit

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -4,10 +4,12 @@ import { View, Text, Button } from 'react-native';
 export const ContadorScreen = () => {
 
     const [contador, setContador] = useState(10)
+    const [number, setNumber] = useState(1)
+
     return (
         <View style={{
             flex: 1,
-            justifyContent: "center"
+            justifyContent: "center",
         }}>
             <Text style={{
                 textAlign: 'center',
@@ -15,8 +17,15 @@ export const ContadorScreen = () => {
                 top:-20,
             }}> Contador: {contador}
             </Text>
+            
+            <Text style={{textAlign: 'center',
+                fontSize: 40}}>
+                Numero Aleatorio: {number}
+            </Text>
 
-            <Button title='Click' onPress={() =>setContador(contador+1)}></Button>
+            <Button title='Suma' onPress={() =>setContador(contador+1)}></Button>
+            <Button title='Resta' onPress={() =>setContador(contador-1)}></Button>
+            <Button title='Random number' onPress={() =>setNumber(Math.floor(Math.random()*777))}></Button>
 
 
         </View>
