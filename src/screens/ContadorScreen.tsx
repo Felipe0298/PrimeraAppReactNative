84cf09a86bee7f1c8021713a85b62817e96df791
Felipe Hoyos <felipe02-98@hotmail.com>
2023-02-23 18:57:39 -0500

    Boton = TouchableOpacity

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Touchable, TouchableOpacity } from 'react-native';
 
 export const ContadorScreen = () => {
 
@@ -14,19 +14,18 @@ export const ContadorScreen = () => {
             <Text style={{
                 textAlign: 'center',
                 fontSize: 40,
-                top:-20,
+                top: -20,
             }}> Contador: {contador}
             </Text>
-            
-            <Text style={{textAlign: 'center',
-                fontSize: 40}}>
-                Numero Aleatorio: {number}
-            </Text>
 
-            <Button title='Suma' onPress={() =>setContador(contador+1)}></Button>
-            <Button title='Resta' onPress={() =>setContador(contador-1)}></Button>
-            <Button title='Random number' onPress={() =>setNumber(Math.floor(Math.random()*777))}></Button>
-
+            <TouchableOpacity onPress={() => setContador(contador + 1)}>
+                <View style={{
+                    backgroundColor: "red",
+                    borderRadius: 100
+                }}>
+                    <Text>Sumar</Text>
+                </View>
+            </TouchableOpacity>
 
         </View>
     )
